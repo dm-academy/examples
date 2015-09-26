3e23b13cf178e2cb3d869016c71e3ed9d0362fec
suprajamano <supraja.manoharan@gmail.com>
2015-09-26 01:05:45 +0000

    script commit

---
+++ cagrabitske-Lab02.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Starter shell script
+# Rename as YourID-Lab02.sh
+# Put commands below
+# To run, type ./YourID-Lab02.sh (you need the "./")
+# It must have permissions starting with a "7"
+mkdir A
+touch ./A/aa
+mkdir ./A/B
+touch ./A/B/bb
+mkdir ./A/C
+touch ./A/C/cc
+mkdir ./A/D
+mkdir ./A/D/dd
+mkdir ./C
+touch ./C/cc
+mkdir ./C/E
+touch ./C/E/ee
+mkdir ./C/F
+touch ./C/F/ff
+mkdir ./D
+touch ./D/dd
+mkdir ./D/G
+touch ./D/G/gg
+mkdir ./D/H
+touch ./D/H/hh
+mkdir ./D/I
+touch ./D/I/ii
+mkdir ./D/H/J
+mkdir ./D/H/K
+touch ./D/H/J/jj
+touch ./D/H/K/kk
+apt-get install -y tree
+apt-get install -y git
