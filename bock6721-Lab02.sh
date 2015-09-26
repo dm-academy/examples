ca12ae18c6ddcbbab2d87daef486bc4094abf3f1
Dan Bocklund <bock6721@stthomas.edu>
2015-09-26 00:54:57 +0000

    script commit

---
+++ bock6721-Lab02.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Starter shell script
+# Put commands below
+# To run, type ./starter.sh (you need the "./")
+# It must have permissions starting with a "7"
+
+mkdir -p main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
+touch main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
+apt-get install -y tree
+apt-get install -y git
