0533daa8ce2190acb4a39e947e6589ebbee2878c
suprajamano <supraja.manoharan@gmail.com>
2015-09-26 01:15:39 +0000

    2nd script commit

--- cagrabitske-Lab02.sh
+++ cagrabitske-Lab02.sh
@@ -4,31 +4,32 @@
 # Put commands below
 # To run, type ./YourID-Lab02.sh (you need the "./")
 # It must have permissions starting with a "7"
-mkdir A
-touch ./A/aa
-mkdir ./A/B
-touch ./A/B/bb
-mkdir ./A/C
-touch ./A/C/cc
-mkdir ./A/D
-mkdir ./A/D/dd
-mkdir ./C
-touch ./C/cc
-mkdir ./C/E
-touch ./C/E/ee
-mkdir ./C/F
-touch ./C/F/ff
-mkdir ./D
-touch ./D/dd
-mkdir ./D/G
-touch ./D/G/gg
-mkdir ./D/H
-touch ./D/H/hh
-mkdir ./D/I
-touch ./D/I/ii
-mkdir ./D/H/J
-mkdir ./D/H/K
-touch ./D/H/J/jj
-touch ./D/H/K/kk
+mkdir main
+mkdir ./main/A
+touch ./main/A/aa
+mkdir ./main/A/B
+touch ./main/A/B/bb
+mkdir ./main/A/C
+touch ./main/A/C/cc
+mkdir ./main/A/D
+mkdir ./main/A/D/dd
+mkdir ./main/E
+touch ./main/E/ee
+mkdir ./main/E/F
+touch ./main/E/F/ff
+mkdir ./main/E/G
+touch ./main/E/G/gg
+mkdir ./main/H
+touch ./main/H/hh
+mkdir ./main/H/I
+touch ./main/H/I/ii
+mkdir ./main/H/J
+touch ./main/H/J/jj
+mkdir ./main/H/J/K
+touch ./main/H/J/K/kk
+mkdir ./main/H/J/L
+touch ./main/H/J/L/ll
+touch ./main/H/M
+touch ./main/H/M/mm
 apt-get install -y tree
 apt-get install -y git
