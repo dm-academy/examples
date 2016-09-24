c4fa1f8ca6f6132d728cc6298531435ad6cc94c9
vagrant <vagrant@precise64.(none)>
2016-09-24 01:04:52 +0000

    2nd script commit

---
+++ 101057650-Lab02.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Starter shell script
+# Rename as YourID-Lab02.sh
+# Put commands below
+# To run, type ./YourID-Lab02.sh (you need the "./")
+# It must have permissions starting with a "7"
+mkdir main
+cd main
+mkdir A
+mkdir E
+mkdir H
+cd A
+touch aa
+mkdir B
+mkdir C
+mkdir D
+cd B
+touch bb
+cd ..
+cd C
+touch cc
+cd ..
+cd D
+touch dd
+cd ..
+cd ..
+cd E
+mkdir F
+mkdir G
+touch ee
+cd F
+touch ff
+cd ..
+cd G
+touch gg
+cd ..
+cd ..
+cd H
+touch hh
+mkdir I
+mkdir J
+mkdir M
+cd I
+touch ii
+cd ..
+cd J
+touch jj
+mkdir K
+mkdir L
+cd K
+touch kk
+cd ..
+cd L
+touch ll
+cd ..
+cd ..
+cd M
+touch mm
+
+apt-get install -y tree
+apt-get install -y git 
+
