5759a68eea8284eae17eeadf7ded1e3ca4e8f564
Fahd <alha0007@stthomas.edu>
2016-09-24 03:05:50 +0000

    2nd script commit

--- alha0007-Lab02.sh
+++ alha0007-Lab02.sh
@@ -9,3 +9,4 @@ mkdir -p main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
 touch main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
 apt-get -y install tree
 apt-get -y install git
+
