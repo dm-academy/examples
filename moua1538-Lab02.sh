0ec1598f3a56d0c3887b6a8deb0f49e71c73b888
Tsongfw Moua <moua1538@stthomas.edu>
2016-09-24 01:42:38 +0000

    script commit

---
+++ moua1538-Lab02.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Starter shell script
+# Rename as YourID-Lab02.sh
+# Put commands below
+# To run, type ./YourID-Lab02.sh (you need the "./")
+# It must have permissions starting with a "7"
+mkdir A C D
+cd A
+touch aa
+mkdir B C D
+cd B
+touch bb
+cd ..
+cd C
+touch cc
+cd ..
+cd D
+touch dd
+cd ..
+cd ..
+cd C
+touch cc
+mkdir E F
+cd E
+touch ee
+cd ..
+cd F
+touch ff
+cd ..
+cd ..
+cd D
+touch dd
+mkdir G H I
+cd G
+touch gg
+cd ..
+cd H
+touch hh
+cd ..
+cd I
+touch ii
+cd ..
+cd H
+mkdir J K
+cd J
+touch jj
+cd ..
+cd K
+touch kk
+cd
+
+apt-get install -y tree
+apt-get install -y git
+
