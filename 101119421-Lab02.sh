0f22b3d07addca55ce402d55cc41cc924b7c91a1
Hissah1 <altu1087@stthomas.edu>
2016-09-25 05:31:51 +0000

    script commit

---
+++ 101119421-Lab02.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Starter shell script
+# Rename as YourID-Lab02.sh
+# Put commands below
+# To run, type ./YourID-Lab02.sh (you need the "./")
+# It must have permissions starting with a "7"
+pwd
+ls
+echo "What is your name?"
+read PERSON
+echo "Hello, $PERSON"
+
