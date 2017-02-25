da90164a4c92c332f8fa23297c2f379ca733bdb9
Asseri <asse0010@stthomas.edu>
2017-02-25 02:27:02 +0000

    script commit

--- asse0010-Lab02.sh
+++ asse0010-Lab02.sh
@@ -4,7 +4,7 @@
 # Put commands below
 # To run, type ./YourID-Lab02.sh (you need the "./")
 # It must have permissions starting with a "7"
-
-apt-get install -y tree
-apt-get install -y git
-
+mkdir -p main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
+touch -p main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
+apt-get install tree
+apt-get install git
