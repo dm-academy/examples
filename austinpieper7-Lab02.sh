45ef91507d8eea15c6171e1fbc4365da5d612c7e
Austin Pieper <piep9659@stthomas.edu>
2017-02-24 19:29:53 -0600

    Vagrantfile commmit 2

--- austinpieper7-Lab02.sh
+++ austinpieper7-Lab02.sh
@@ -5,8 +5,8 @@
 # To run, type ./YourID-Lab02.sh (you need the "./")
 # It must have permissions starting with a "7"
 
-mkdir -p mail/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
-touch main/A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
+mkdir -p main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
+touch main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
 
 apt-get -y install tree
-apt0get -y install git
+apt-get -y install git
