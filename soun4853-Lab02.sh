980267f5753fe4cea42250f97a9713ab8073a1fc
Revenmoore <soun4853@stthomas.edu>
2017-02-25 01:35:09 +0000

    script commit

---
+++ soun4853-Lab02.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Starter shell script
+# Rename as YourID-Lab02.sh
+# Put commands below
+# To run, type ./YourID-Lab02.sh (you need the "./")
+# It must have permissions starting with a "7"
+echo Hello World
+
