08eb48dd218923e66442aaf96b122ac845106f03
Travis Gehling <trgehling@stthomas.edu>
2017-09-22 20:09:50 -0500

    100464343-Lab02.sh commit 2

--- 100464343-Lab02.sh
+++ 100464343-Lab02.sh
@@ -5,8 +5,8 @@
 # To run, type ./YourID-Lab02.sh (you need the "./")
 # It must have permissions starting with a "7"
 
-apt-get install -y tree
-apt-get install -y git
+#apt-get install -y tree
+#apt-get install -y git
 
 mkdir ~/main
 cd ~/main
@@ -15,3 +15,5 @@ mkdir E E/F E/G
 mkdir H H/I H/J H/J/K H/J/L H/M
 touch A/aa A/B/bb A/C/cc A/D/dd E/ee E/F/ff E/G/gg H/hh H/I/ii H/J/jj H/J/K/kk H/J/L/ll H/M/mm
 
+apt-get install -y tree
+apt-get install -y git
