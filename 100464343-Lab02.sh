e5c9837d6dd9e520fd3ba73bcbc4da45b4855d54
Travis Gehling <trgehling@stthomas.edu>
2017-09-23 00:43:15 +0000

    script commit

---
+++ 100464343-Lab02.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Starter shell script
+# Rename as YourID-Lab02.sh
+# Put commands below
+# To run, type ./YourID-Lab02.sh (you need the "./")
+# It must have permissions starting with a "7"
+
+apt-get install -y tree
+apt-get install -y git
+
+
+mkdir A A/B A/C A/D
+mkdir C C/E C/F
+mkdir D D/G D/H D/H/J D/H/K D/I
+touch A/aa C/cc D/dd A/B/bb A/C/cc A/D/dd C/E/ee C/F/ff D/G/gg D/H/hh D/I/ii D/H/J/jj D/H/K/kk
+
