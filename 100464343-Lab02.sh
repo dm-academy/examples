8265f86657af534759115f9a398af0d491d83af2
Travis Gehling <trgehling@stthomas.edu>
2017-09-23 00:52:14 +0000

    2nd script commit

--- 100464343-Lab02.sh
+++ 100464343-Lab02.sh
@@ -8,9 +8,10 @@
 apt-get install -y tree
 apt-get install -y git
 
-
+mkdir ~/main
+cd ~/main
 mkdir A A/B A/C A/D
-mkdir C C/E C/F
-mkdir D D/G D/H D/H/J D/H/K D/I
-touch A/aa C/cc D/dd A/B/bb A/C/cc A/D/dd C/E/ee C/F/ff D/G/gg D/H/hh D/I/ii D/H/J/jj D/H/K/kk
+mkdir E E/F E/G
+mkdir H H/I H/J H/J/K H/J/L H/M
+touch A/aa A/B/bb A/C/cc A/D/dd E/ee E/F/ff E/G/gg H/hh H/I/ii H/J/jj H/J/K/kk H/J/L/ll H/M/mm
 
