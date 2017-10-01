101b2dbd829efd65cc300f623e6ef403bb23b6b3
aaronmleff <aaronleff@uwalumni.com>
2017-10-01 00:35:05 +0000

    script commit again

--- leff7766-Lab02.sh
+++ leff7766-Lab02.sh
@@ -1,15 +1,14 @@
+#!/bin/bash
+# Starter shell script
 # Rename as YourID-Lab02.sh
 # Put commands below
 # To run, type ./YourID-Lab02.sh (you need the "./")
 # It must have permissions starting with a "7"
 
-#creating those directories/folders form part 1
-mkdir -p ~/main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
-touch ~/main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll}M/mm}}
+#creating those directories ....
+mkdir -p /{A/{B,C,D},C/{E,F},D/{G,H/{J,K},I}}
+touch /{A/{aa,B/bb,C/cc,D/dd},C/{cc,E/ee,F/ff},D/{dd,G/gg,H/{hh,J/jj,K/kk},I/ii}}
 
-
-#installing those tree and git updates
-apt-get update
+#updates
 apt-get -y install tree
 apt-get -y install git
-
