d37eaed6a05c23859eb5bc7d4290726459e2b15b
Vicky <vue07418@stthomas.edu>
2018-02-10 06:02:36 +0000

    2nd script commit

--- vue07418-Lab02.sh
+++ vue07418-Lab02.sh
@@ -5,11 +5,11 @@
 # To run, type ./YourID-Lab02.sh (you need the "./")
 # It must have permissions starting with a "7"
 
-mkdir -p /vagrant/A/{aa,B/bb,C/cc,D/dd}
+mkdir -p /vagrant/main/A/{aa,B/bb,C/cc,D/dd}
 
-mkdir -p /vagrant/C/{cc,E/ee,F/ff}
+mkdir -p /vagrant/main/E/{ee,F/ff,G/gg}
 
-mkdir -p /vagrant/D/{dd,G/gg,H/{hh,J/jj,K/kk},I/ii}
+mkdir -p /vagrant/main/H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}
 
 
 tree
