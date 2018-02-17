8618c0489c61ec40195482d653c5d35c1dbb07de
Vicky <vue07418@stthomas.edu>
2018-02-17 01:23:41 +0000

    2nd script commit

--- vue07418-Lab02.sh
+++ vue07418-Lab02.sh
@@ -7,3 +7,5 @@
 
 mkdir -p /vagrant/main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
 touch /vagrant/main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
+apt-get install -y tree
+apt-get install -y git
