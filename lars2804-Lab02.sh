2842b6aa1b1bfa1e653396e88da13bc57e6d3255
adrianelarson <lars2804@stthomas.edu>
2018-02-16 20:04:51 -0600

    3rd script commit

--- lars2804-Lab02.sh
+++ lars2804-Lab02.sh
@@ -5,25 +5,9 @@
 # To run, type ./YourID-Lab02.sh (you need the "./")
 # It must have permissions starting with a "7"
 #**create files**
-mkdir {A,E,H}
-mkdir -p A/{B,C,D}
-mkdir -p E/{F,G}
-mkdir -p H/{I,J,M}
-mkdir -p  H/J/{K,L}
-touch /vagrant/A/aa
-touch /vagrant/E/ee
-touch /vagrant/H/hh
-touch /vagrant/A/B/bb
-touch /vagrant/A/C/cc
-touch /vagrant/A/D/dd
-touch /vagrant/E/F/ff
-touch /vagrant/E/G/gg
-touch /vagrant/H/I/ii
-touch /vagrant/H/J/jj
-touch /vagrant/H/M/mm
-touch /vagrant/H/J/K/kk
-touch /vagrant/H/J/L/ll
-#***install tree***
+mkdir -p ~/main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
+touch ~/main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
+#***install***
 apt-get update
 apt-get install -y tree
 apt-get install -y git
