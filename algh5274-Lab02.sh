13c9ad71bf98b9573ba8f8b651bef018a3a57742
Itidal <queen_3dooola@hotmail.com>
2018-02-24 06:29:47 +0000

    script commit

--- algh5274-Lab02.sh
+++ algh5274-Lab02.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 # Starter shell script
-# Rename as algh5274-Lab02.sh
+# Rename as YourID-Lab02.sh
 # Put commands below
-# To run, type ./algh5274-Lab02.sh (you need the "./")
+# To run, type ./YourID-Lab02.sh (you need the "./")
 # It must have permissions starting with a "7"
-sudo apt-get update
-mkdir -p vagrant/main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
-touch vagrant/main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
-apt-get -y install tree
-apt-get -y install git
-
