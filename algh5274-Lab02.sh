c607efc74335df458c704dbf32b8449ec45fba80
Itidal <queen_3dooola@hotmail.com>
2018-02-24 06:04:41 +0000

    script commit

--- algh5274-Lab02.sh
+++ algh5274-Lab02.sh
@@ -5,5 +5,8 @@
 # To run, type ./algh5274-Lab02.sh (you need the "./")
 # It must have permissions starting with a "7"
 sudo apt-get update
-apt-get install -y tree
-apt-get install -y git
+mkdir -p vagrant/main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
+touch vagrant/main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
+apt-get -y install tree
+apt-get -y install git
+
