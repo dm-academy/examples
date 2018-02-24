c3aad1bb611459a75b4383ccdf97a26e91d09d6a
Itidal <queen_3dooola@hotmail.com>
2018-02-24 05:03:00 +0000

    script commit

---
+++ algh5274-Lab02.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Starter shell script
+# Rename as algh5274-Lab02.sh
+# Put commands below
+# To run, type ./algh5274-Lab02.sh (you need the "./")
+# It must have permissions starting with a "7"
+sudo apt-get update
+apt-get install -y tree
+apt-get install -y git
