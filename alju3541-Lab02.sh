006d5dcca85d48d79dc6c49447efce1680a1ea7b
ajulayfi <ajulayfi@gmail.com>
2018-02-24 06:59:59 +0000

    2nd script commit

--- alju3541-Lab02.sh
+++ alju3541-Lab02.sh
@@ -4,4 +4,8 @@
 # Put commands below
 # To run, type ./YourID-Lab02.sh (you need the "./")
 # It must have permissions starting with a "7"
-
+sudo apt-get update
+mkdir -p main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
+touch main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
+apt-get install -y tree
+apt-get install -y git
