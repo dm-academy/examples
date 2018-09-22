d4273b691a1d0dcdb748b93c8434e3d0a5578221
AlfiGoyal <goya8893@stthomas.edu>
2018-09-22 00:57:06 +0000

    script commit

---
+++ goya8893-Lab02.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Starter shell script
+# Rename as YourID-Lab02.sh
+# Put commands below
+# To run, type ./goya8893-Lab02.sh (you need the "./")
+# It must have permissions starting with a "7"
+
+mkdir -p main/{A/{B,C,D},C/{E,F},D/{G,H/{J,K},I}}
+touch main/{A/{aa,B/bb,C/cc,D/dd},C/{cc,E/ee,F/ff},D/{dd,G/gg,H/{hh,J/jj,K/kk},I/ii}}
+apt-get -y install tree
+apt-get -y install git
+
