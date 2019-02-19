8f85f4f7c7424c98d8e45a45a178255ea9f43464
Su <dogr0001@stthomas.edu>
2019-02-19 15:27:42 +0000

    script commit

--- dogr0001-Lab02.sh
+++ dogr0001-Lab02.sh
@@ -4,7 +4,7 @@
 # Put commands below
 # To run, type ./YourID-Lab02.sh (you need the "./")
 # It must have permissions starting with a "7"
-mkdir -p main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
+mkdir -p ~/main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
 touch ~/main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
 sudo apt-get update
 sudo apt-get install -y tree
