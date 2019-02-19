243a2a689b472892ca3013c68638e9f5c4bac927
Su <dogr0001@stthomas.edu>
2019-02-19 04:40:04 +0000

    script commit

--- dogr0001-Lab02.sh
+++ dogr0001-Lab02.sh
@@ -2,10 +2,10 @@
 # Starter shell script
 # Rename as dogr0001-Lab02.sh
 # Put commands below
-mkdir -p ~/main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
-touch ~/main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
-#sudo apt-get update
-sudo apt-get install -y tree
-apt-get -y install git
 # To run, type ./YourID-Lab02.sh (you need the "./")
 # It must have permissions starting with a "7"
+mkdir -p main/{A/{B,C,D},C/{E,F},D/{G,H/{J,K},I}}
+touch main/{A/{aa,B/bb,C/cc,D/dd},C/{cc,E/ee,F/ff},D/{dd,G/gg,H/{hh,J/jj,K/kk},I/ii}}
+sudo apt-get update
+sudo apt-get install -y tree
+apt-get -y install git
