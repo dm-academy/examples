02112c64993b90c71758955cdf3b6a58450a2e45
Nirma Parajuli <nirma.parajuli@stthomas.edu>
2019-02-22 22:43:25 +0000

    script commit

---
+++ para4100-Lab02.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+apt-get install -y tree
+apt-get install -y git
+# Starter shell script
+# Rename as YourID-Lab02.sh
+# Put commands below
+# To run, type ./YourID-Lab02.sh (you need the "./")
+# It must have permissions starting with a "7"
