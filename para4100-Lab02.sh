fef14838f99b1d4d59662d68cc4d252123d68d5c
Nirma Parajuli <nirma.parajuli@stthomas.edu>
2019-02-24 05:35:53 +0000

    2nd script commit to make tree

--- para4100-Lab02.sh
+++ para4100-Lab02.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-# Starter shell script
-apt-get install -y tree
-apt-get intall -y git
-# Rename as YourID-Lab02.sh
-# Put commands below
-# To run, type ./YourID-Lab02.sh (you need the "./")
-# It must have permissions starting with a "7"
+mkdir main
+cd main
+mkdir A E H
+cd A 
+mkdir B C D
+touch aa
+touch B/bb
+touch C/cc
+touch D/dd
+cd ../E
+mkdir F G
+touch ee F/ff G/gg
+cd ../H
+mkdir I J M
+touch hh I/ii J/jj M/mm
+
+
